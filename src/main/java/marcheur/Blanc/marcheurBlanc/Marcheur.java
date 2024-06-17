91df29008f7ee77fee3f4947b06a465a6be04311
hei.marius.3@gmail.com <hei.marius.3@gmail.com>
2024-06-17 16:54:37 +0300

    feat: methode marche in marcheur class

--- src/main/java/marcheur/Blanc/marcheurBlanc/Marcheur.java
+++ src/main/java/marcheur/Blanc/marcheurBlanc/Marcheur.java
@@ -1,2 +1,25 @@
-package marcheur.Blanc.marcheurBlanc;public class Marcheur {
+package marcheur.Blanc.marcheurBlanc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Marcheur {
+    private Carte carte;
+    private Random random;
+
+    public List<Lieux> marche(Lieux lieuDepart, Lieux lieuDestination) {
+        List<Lieux> chemin = new ArrayList<>();
+        Lieux lieuActuel = lieuDepart;
+
+        while (!lieuActuel.equals(lieuDestination)) {
+            List<Lieux> voisins = carte.getVoisins(lieuActuel);
+            if (voisins.isEmpty()) break;
+            Lieux prochainLieu = voisins.get(random.nextInt(voisins.size()));
+            chemin.add(prochainLieu);
+            lieuActuel = prochainLieu;
+        }
+
+        return chemin;
+    }
 }
