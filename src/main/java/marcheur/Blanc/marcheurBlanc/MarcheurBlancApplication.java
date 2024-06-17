fd05cbeda7e017910fa4132c55542090a6b40cb6
hei.marius.3@gmail.com <hei.marius.3@gmail.com>
2024-06-17 16:31:02 +0300

    firts commit

---
+++ src/main/java/marcheur/Blanc/marcheurBlanc/MarcheurBlancApplication.java
@@ -0,0 +1,13 @@
+package marcheur.Blanc.marcheurBlanc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcheurBlancApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcheurBlancApplication.class, args);
+	}
+
+}
---
+++ src/test/java/marcheur/Blanc/marcheurBlanc/MarcheurBlancApplicationTests.java
@@ -0,0 +1,13 @@
+package marcheur.Blanc.marcheurBlanc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarcheurBlancApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
